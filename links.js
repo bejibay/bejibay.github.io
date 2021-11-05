8e7277b93d35dd1486c25f68127eaa98a7015b60
bejibay <58639255+bejibay@users.noreply.github.com>
2021-11-05 16:21:28 +0100

    Update links.js

--- links.js
+++ links.js
@@ -1,5 +1,5 @@
 function topLinks(){
 var x=document.getElementById("myLinks");
-if(x.style.display==="block"){x.style.display==="none";}
-else{x.style.display==="block";}
+if(x.style.display==="block"){x.style.display="none";}
+else{x.style.display="block";}
 }
